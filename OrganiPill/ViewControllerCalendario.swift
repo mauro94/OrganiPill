2dc182c4e5903cf30e12ba1f8011de99215e7f46
Mauro Amarante <mauro.amarante94@gmail.com>
2016-04-13 20:18:58 -0500

    Numero del dia agregado

--- OrganiPill/ViewControllerCalendario.swift
+++ OrganiPill/ViewControllerCalendario.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-@IBDesignable class ViewControllerCalendario: UIViewController {
+class ViewControllerCalendario: UIViewController {
 	//outlets
+	//botones de cada dia
 	@IBOutlet weak var btDomingo: UIButton!
 	@IBOutlet weak var btLunes: UIButton!
 	@IBOutlet weak var btMartes: UIButton!
@@ -18,9 +19,20 @@ import UIKit
 	@IBOutlet weak var btViernes: UIButton!
 	@IBOutlet weak var btSabado: UIButton!
 	
+	//labels de numero de dia
+	@IBOutlet weak var lbDomingo: UILabel!
+	@IBOutlet weak var lbLunes: UILabel!
+	@IBOutlet weak var lbMartes: UILabel!
+	@IBOutlet weak var lbMiercoles: UILabel!
+	@IBOutlet weak var lbJueves: UILabel!
+	@IBOutlet weak var lbViernes: UILabel!
+	@IBOutlet weak var lbSabado: UILabel!
+	
 	//variables
-	var clBoton: UIColor = UIColor(red: 255.0/255.0, green: 70.0/255.0, blue: 89.0/255.0, alpha: 1)
+	let clBoton: UIColor = UIColor(red: 255.0/255.0, green: 70.0/255.0, blue: 89.0/255.0, alpha: 1)
 	var botonesDias = [UIButton]()
+	var lbNumeroDias = [UILabel]()
+	
 	
 
     override func viewDidLoad() {
@@ -35,6 +47,45 @@ import UIKit
 		botonesDias.append(btJueves)
 		botonesDias.append(btViernes)
 		botonesDias.append(btSabado)
+		
+		//crear arreglos de labels de numeros de dias
+		lbNumeroDias.append(lbDomingo)
+		lbNumeroDias.append(lbLunes)
+		lbNumeroDias.append(lbMartes)
+		lbNumeroDias.append(lbMiercoles)
+		lbNumeroDias.append(lbJueves)
+		lbNumeroDias.append(lbViernes)
+		lbNumeroDias.append(lbSabado)
+		
+		//obtener el nombre del dia de hoy
+		let dateFechaHoy = NSDate()
+		let dateFormatter = NSDateFormatter()
+		dateFormatter.locale = NSLocale(localeIdentifier: "es")
+		dateFormatter.dateFormat = "EEEE"
+		let diaDeLaSemana = dateFormatter.stringFromDate(dateFechaHoy)
+		
+		//actualizar numero de los dias
+		actualizaNumeroDias(diaDeLaSemana, dateFechaHoy: dateFechaHoy, dateFormatter: dateFormatter)
+		
+		//encender boton del dia de hoy
+		switch diaDeLaSemana {
+		case "domingo":
+			btPresionarBotonDia(btDomingo)
+		case "lunes":
+			btPresionarBotonDia(btLunes)
+		case "martes":
+			btPresionarBotonDia(btMartes)
+		case "miércoles":
+			btPresionarBotonDia(btMiercoles)
+		case "jueves":
+			btPresionarBotonDia(btJueves)
+		case "viernes":
+			btPresionarBotonDia(btViernes)
+		case "sabádo":
+			btPresionarBotonDia(btSabado)
+		default:
+			print("ERROR")
+		}
 		
 		//llama funcion que agrega UI a los botones
 		agregarUIBoton()
@@ -54,9 +105,16 @@ import UIKit
 			boton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
 		}
 		
+		for label in lbNumeroDias {
+			label.textColor = UIColor.blackColor()
+		}
+		
 		//boton seleccionado cambiar color
 		sender.layer.backgroundColor = clBoton.CGColor
 		sender.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+		
+		let iUbicacionArreglo = botonesDias.indexOf(sender as! UIButton)
+		lbNumeroDias[iUbicacionArreglo!].textColor = UIColor.whiteColor()
 	}
 	
 	
@@ -73,18 +131,88 @@ import UIKit
 		for constraint in constraints {
 			if (constraint.identifier == "btAltura") {
 				constraint.constant = view.bounds.width/7.0
-				print(view.bounds.width/7.0)
 			}
 		}
-
+		
+		//agregar numero de dia si iphone 6 o mas grande
+		if (view.frame.width >= 375) {
+			//bajar letra del dia para poner numero del dia
+			for boton in botonesDias {
+				boton.contentVerticalAlignment = UIControlContentVerticalAlignment.Bottom
+			}
+			//hacer numeros de dias aparecer
+			for label in lbNumeroDias {
+				label.hidden = false
+			}
+		}
 	}
 	
 	//funcion que agregar bordes
 	func agregaBorderButton(sender: UIButton) {
-		sender.layer.borderWidth = 1
+		sender.layer.borderWidth = 0.5
 		sender.layer.borderColor = UIColor.blackColor().CGColor
 	}
-    
+	
+	func actualizaNumeroDias(diaDeLaSemana: String, dateFechaHoy: NSDate, dateFormatter: NSDateFormatter) {
+		//variables
+		var iUbicacionArreglo: Int = 0
+		var i: Int = 0
+		var numeroDiaHoy: String
+		
+		//formato de dias
+		dateFormatter.dateFormat = "dd"
+		
+		//definir ubicacion actual en el arreglo de dias
+		switch diaDeLaSemana {
+			case "domingo":
+				iUbicacionArreglo = 0
+			case "lunes":
+				iUbicacionArreglo = 1
+			case "martes":
+				iUbicacionArreglo = 2
+			case "miércoles":
+				iUbicacionArreglo = 3
+			case "jueves":
+				iUbicacionArreglo = 4
+			case "viernes":
+				iUbicacionArreglo = 5
+			case "sabádo":
+				iUbicacionArreglo = 6
+			default:
+				print("ERROR")
+		}
+		
+		//almacenar numero de dia hoy
+		numeroDiaHoy = dateFormatter.stringFromDate(dateFechaHoy)
+		lbNumeroDias[iUbicacionArreglo].text = numeroDiaHoy
+		
+		//inicir un dia despues
+		i = iUbicacionArreglo + 1
+		
+		//sumar dias
+		while (i <= 6) {
+			let dateTemp = NSDate(timeIntervalSinceNow: Double(i - iUbicacionArreglo)*24*60*60)
+			
+			numeroDiaHoy = dateFormatter.stringFromDate(dateTemp)
+			
+			lbNumeroDias[i].text = numeroDiaHoy
+			i += 1
+		}
+		
+		i = iUbicacionArreglo
+		
+		//restar dias
+		while (i >= 0) {
+			let dateTemp = NSDate(timeIntervalSinceNow: Double(iUbicacionArreglo - i)*(-24)*60*60)
+			
+			numeroDiaHoy = dateFormatter.stringFromDate(dateTemp)
+			
+			lbNumeroDias[i].text = numeroDiaHoy
+			i -= 1
+		}
+	}
+	
+	
 
     /*
     // MARK: - Navigation
