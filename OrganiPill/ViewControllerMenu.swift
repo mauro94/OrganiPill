37b305d7d93441f031f3d6328ecce8d7b3f7059a
gonzalogtz <gonzalogtzs94@gmail.com>
2016-05-04 20:38:16 -0500

    Mail
    mail reportes

--- OrganiPill/ViewControllerMenu.swift
+++ OrganiPill/ViewControllerMenu.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewControllerMenu: UIViewController {
+class ViewControllerMenu: UIViewController,MFMailComposeViewControllerDelegate {
 	//outlet
 	@IBOutlet weak var btMisMedicamentos: UIButton!
 	@IBOutlet weak var btCalendario: UIButton!
@@ -84,6 +85,48 @@ class ViewControllerMenu: UIViewController {
 	@IBAction func highlightBoton(sender: UIButton) {
 		sender.setBackgroundImage(UIImage(named: "highlightMenu"), forState: UIControlState.Highlighted)
 	}
+    
+    
+    
+    
+    
+    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        }
+        else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["gonzalogtzs94@gmail.com"])
+        mailComposerVC.setSubject("OrganiPill")
+        mailComposerVC.setMessageBody("<p>Holaaa<p>", isHTML: true)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
+    
     
     func alertaNotificacion(nombreMed : String, fecha : NSDate, notification : UILocalNotification){
         let alerta = UIAlertController(title: "Alerta!", message: "Hora de tomar \(nombreMed))", preferredStyle: UIAlertControllerStyle.Alert)
--- OrganiPill/ViewControllerVerMedicamento.swift
+++ OrganiPill/ViewControllerVerMedicamento.swift
@@ -123,43 +123,7 @@ class ViewControllerVerMedicamento: UIViewController, MFMailComposeViewControlle
         
     }
     
-    // --------------------------- MAIL --------------------------------------------------
-
-    
-    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
-        let mailComposeViewController = configuredMailComposeViewController()
-        if MFMailComposeViewController.canSendMail() {
-            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
-        }
-		else {
-            self.showSendMailErrorAlert()
-        }
-    }
-    
-    
-    
-    func configuredMailComposeViewController() -> MFMailComposeViewController {
-        let mailComposerVC = MFMailComposeViewController()
-        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
-        
-        mailComposerVC.setToRecipients(["gonzalogtzs94@gmail.com"])
-        mailComposerVC.setSubject("OrganiPill")
-        mailComposerVC.setMessageBody("<p>Holaaa<p>", isHTML: true)
-        
-        return mailComposerVC
-    }
-    
-    func showSendMailErrorAlert() {
-        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
-        sendMailErrorAlert.show()
-    }
-    
-    // MARK: MFMailComposeViewControllerDelegate Method
-    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
-        controller.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    //--------------------------- MAIL --------------------------------------------------
+ 
 	
 	
 	func cambiarFoto(swipe: UISwipeGestureRecognizer) {
